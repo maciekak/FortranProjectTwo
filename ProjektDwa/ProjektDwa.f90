06251e9e5cfb49c43ad6623ca0065aa26c7d103d
Maciej Klimowski <maciek.klimowski44@gmail.com>
2018-06-03 14:50:43 +0200

    Add project files.

---
+++ ProjektDwa/ProjektDwa.f90
@@ -0,0 +1,25 @@
+!  ProjektDwa.f90 
+!
+!  FUNCTIONS:
+!  ProjektDwa - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: ProjektDwa
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program ProjektDwa
+
+    implicit none
+
+    ! Variables
+
+    ! Body of ProjektDwa
+    print *, 'Hello World'
+
+    end program ProjektDwa
+
